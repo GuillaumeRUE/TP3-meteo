266cedc347307d3ad92a0610b9fee22b491704e7
Guillaume <grue57@yahoo.fr>
2019-03-27 14:00:18 +0100

    init

---
+++ src/main/java/fr/ul/miage/rue/TP3/App.java
@@ -0,0 +1,14 @@
+package fr.ul.miage.rue.TP3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
