0d42df5d2627237598ba8ab2c389ca9cf657a043
Guillaume <grue57@yahoo.fr>
2019-03-30 11:00:04 +0100

    Creation de la vue settings

---
+++ src/main/java/fr/ul/miage/rue/TP3/Main.java
@@ -0,0 +1,16 @@
+package fr.ul.miage.rue.TP3;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/main/java/fr/ul/miage/rue/TP3/SettingsController.java
@@ -0,0 +1,31 @@
+package fr.ul.miage.rue.TP3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SettingsController {
+	
+	@FXML
+	Button validateButton;
+	
+	@FXML
+	Button quitButton;
+	
+	@FXML
+	TextField cityName;
+	
+	@FXML
+	TextField reloadTime;
+	
+	@FXML
+	public void onClickValideronClickValider() {
+		
+	}
+	
+	@FXML
+	public void OnClickQuit() {
+		
+	}
+
+}
